2aa154a230f0ee2f057a37862c63c12045620ea6
Brunno Quadrado <bbertoninuti@gmail.com>
2024-08-12 12:49:38 -0300

    criação de construtores e novos metodos

--- entities/Order.java
+++ entities/Order.java
@@ -1,14 +1,90 @@
 package entities;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import entities.enums.OrderStatus;
 
 public class Order {
 
+	public static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
 	private Date moment;
 	private OrderStatus status;
 	
 	private Client client;
+	
+	private List<OrderItem> items = new ArrayList<>();
+	
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getOrderItem() {
+		return items;
+	}
+	
+	public void addItem(List<OrderItem> orderItem) {
+		orderItem.add((OrderItem) orderItem);
+	}
+	
+	public void removeItem(List<OrderItem> orderItem) {
+		orderItem.remove(orderItem);
+	}
+	
+	public double total() {
+		double sum = 0;
+		for (OrderItem item: items) {
+			sum += item.subTotal();
+		}
+		return sum;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("ORDER SUMMARY: \n");
+		sb.append("Order Moment: " + sdf.format(moment) + "\n");
+		sb.append("Order Status: " + status + "\n");
+		sb.append("Client: " + client);
+		sb.append("Order items: ");
+		for (OrderItem item: items) {
+			System.out.println(item);
+		}
+		sb.append("Total price:" + total());
+		return sb.toString();
+	}
+	
+	
 	
 }
--- entities/OrderItem.java
+++ entities/OrderItem.java
@@ -2,9 +2,59 @@ package entities;
 
 public class OrderItem {
 	
+	private String name;
 	private Integer quantity;
 	private Double price;
 	
 	private Product product;
 
+	public OrderItem() {
+	}
+
+	public OrderItem(String name, Integer quantity, Double price, Product product) {
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	public double subTotal() {
+		return quantity * price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return name + ", " + price + "Quantity: " + quantity + ", Subtotal: " + subTotal();
+	}
 }
